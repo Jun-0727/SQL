bc03f30452ba35459ab22e8121b3cb83e90ac72a
jetty <dlwnsdud980727@gmail.com>
2025-10-23 10:05:09 +0900

    [SQL / 조건문] 대장균 크기에 따른 분류 1

---
+++ Select/299307.sql
@@ -0,0 +1,17 @@
+/* 대장균의 크기에 따라 분류하기 1 */
+
+SELECT ID, CASE 
+    WHEN SIZE_OF_COLONY <= 100 THEN 'LOW' 
+    WHEN SIZE_OF_COLONY >1000 THEN 'HIGH' 
+    ELSE 'MEDIUM' END AS SIZE
+FROM ECOLI_DATA
+ORDER BY ID ASC
+
+/* 
+-- SQL_조건문 --
+CASE 
+    WHEN 조건1 THEN 반환1
+    WHEN 조건2 THEN 반환2
+    ELSE 반환3
+END
+*/
